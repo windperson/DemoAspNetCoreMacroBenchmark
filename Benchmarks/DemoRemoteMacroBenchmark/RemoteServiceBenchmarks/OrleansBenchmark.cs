4f0caf91ac2e54d29ecfef1eb815b456110b2fa2
Isak Pao <windperson@gmail.com>
2024-09-11 10:41:15 +0800

    remove not need cleanup code

--- Benchmarks/DemoRemoteMacroBenchmark/RemoteServiceBenchmarks/OrleansBenchmark.cs
+++ Benchmarks/DemoRemoteMacroBenchmark/RemoteServiceBenchmarks/OrleansBenchmark.cs
@@ -47,7 +47,6 @@ public class OrleansBenchmark : BaseBenchmark
     [GlobalCleanup]
     public async Task CleanupClusterClient()
     {
-        _clusterClient = null!;
         await _host.StopAsync();
     }
 }
